faab8fccbbf9c9845cb281b092211af041273de7
kareem <kareem.abdelnabi.22@gmail.com>
2025-01-14 20:14:55 +0200

    UI Setup

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget , QVBoxLayout
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
+from helper_function.compile_qrc import compile_qrc
+from icons_setup.compiledIcons import *
 from classes.signalViewer import SignalViewer
 import numpy as np
+
+compile_qrc()
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        loadUi('main.ui', self)
         self.setWindowTitle('Realtime Digital Filter Designer')
-        
-        
-        central_widget = QWidget()
-        layout = QVBoxLayout()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
-        x = np.linspace(0, 100 * np.pi, 10000)
-        viewer1 = SignalViewer()
-        viewer1.current_signal = [x , np.sin(x)]
-        viewer2 = SignalViewer()
-        viewer2.current_signal = [x , np.sin(2*x)+2]
-        layout.addWidget(viewer1)
-        layout.addWidget(viewer2)
-
+        self.setWindowIcon(QIcon('icons_setup\icons\logo.png'))
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
